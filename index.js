208a5064b3123fdf5b777508438cf43432985f5c
Evan Solomon <evanlsolomon@gmail.com>
2018-07-19 17:30:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+Hq = 42
+
+function distanceFromHqInBlocks (pickUpLocation){
+  return Math.abs(pickUpLocation - 42);
+}
+
+function distanceFromHqInFeet (pickUpLocation){
+  return Math.abs(pickUpLocation - 42)*264;
+}
+
+function distanceTravelledInFeet (start, destination){
+  return Math.abs(start - destination)*264;
+}
+
+function calculatesFarePrice(start, destination){
+  return Math.abs(start - destination)*264;
+}
